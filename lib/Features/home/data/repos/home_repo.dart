d60e3d087f291d07231e68f5194e922958e68364
youssef <yrda3414@gmail.com>
2023-04-16 02:25:28 +0200

    book entity

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
-import 'package:bookly_app/core/utils/errors/failures.dart';
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,6 +1,6 @@
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
-import 'package:bookly_app/core/utils/errors/failures.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'home_repo.dart';
 
