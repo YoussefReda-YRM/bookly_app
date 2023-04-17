81fb46ff2245d6ad6d6d828959ee8471509a2b17
youssef <yrda3414@gmail.com>
2023-04-17 16:18:05 +0200

    register adapter and open featured box

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 const KPrimaryColor = Color(0xff100B20);
 const KTransitionDuration = Duration(milliseconds: 250);
 const KGtSectraFine = 'GT Sectra Fine';
+const KFeaturedBox = 'featured_box';
