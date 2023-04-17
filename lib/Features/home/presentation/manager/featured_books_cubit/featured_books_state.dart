1a4b5680b3e720a15928fc061f95ff68cda87bcc
youssef <yrda3414@gmail.com>
2023-04-17 22:39:51 +0200

    create featured books cubit state

---
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:equatable/equatable.dart';
+
+// part of 'featured_books_cubit.dart';
+
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  const FeaturedBooksFailure(this.errMessage);
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+
+  const FeaturedBooksSuccess(this.books);
+}
